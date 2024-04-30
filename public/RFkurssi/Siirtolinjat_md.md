de6e7157dd9763d6d150bc30166a44a38de1366b
Vikke82 <vikke.majava@gmail.com>
2024-04-30 15:55:09 +0300

    navbar edit

--- public/RFkurssi/Siirtolinjat_md.md
+++ public/RFkurssi/Siirtolinjat_md.md
@@ -10,24 +10,3 @@ _Kuva 1. Vaihtosähkön aaltoluonne kahden johtimen välillä. Mustat pisteet ed
 
 Siirtolinjan pituuden _d_ suhde vaihtosähkön aallonpituuteen &lambda;:
 d < x\*&lambda;, jossa x on suuruusluokkaa 10~20
-
-## Ominaisuudet
-
-- **Helppokäyttöisyys**: Sovelluksemme on suunniteltu olemaan intuitiivinen.
-- **Monipuolisuus**: Voit käyttää sovellusta monilla laitteilla.
-- **Turvallisuus**: Tietosi on turvassa kanssamme.
-
-## Käyttöohjeet
-
-Aloita kirjautumalla sisään. Navigoi päävalikosta haluamaasi osioon. Jos tarvitset apua, ota yhteyttä tukeemme.
-
-### Tuki
-
-Jos kohtaat ongelmia, ota yhteyttä tukeemme sähköpostitse:
-[support@example.com](mailto:support@example.com)
-
-## Kiitos, että valitsit meidät!
-
-Toivomme, että nautit sovelluksemme käytöstä!
-
-![Kuvaus kuvasta](https://example.com/image.png "Esimerkkikuva")
--- src/NavBa.jsx
+++ src/NavBa.jsx
@@ -12,21 +12,19 @@ function NavBa() {
   const { changeLanguage } = useLanguage();
 
   return (
-    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
         <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link className={styles.navLinkCustom} href="/">
-              Home
-            </Nav.Link>
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Nav.Link href="/">Home</Nav.Link>
 
-            <Nav.Link className={styles.navLinkCustom} href="/courses">
-              Courses
-            </Nav.Link>
-            <Nav.Link className={styles.navLinkCustom} href="/blogs">
-              Blog
-            </Nav.Link>
+            <Nav.Link href="/courses">Courses</Nav.Link>
+            <Nav.Link href="/blogs">Blog</Nav.Link>
 
             <Button
               variant="outline-success"
