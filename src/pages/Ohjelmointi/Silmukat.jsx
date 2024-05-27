c966fe80736463409128929f31b7c2bacf4ac1ae
Majava Ville <vikke.majava@gmail.com>
2024-05-27 14:57:33 +0300

    silmukat2

--- src/pages/Ohjelmointi/Silmukat.jsx
+++ src/pages/Ohjelmointi/Silmukat.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Button, Collapse, Container, Row, Col, Card } from "react-bootstrap";
+import { useState } from "react";
 
 const Silmukat = () => {
+  const [open, setOpen] = useState(false);
   return (
     <Container>
       <h1 className="mt-5">Silmukat C-kielessä</h1>
@@ -10,12 +12,26 @@ const Silmukat = () => {
         vähentää virheitä, koodin määrää ja uudelleen käyttää koodia.
       </p>
       <p className="mb-4">
-        Toistuvat tehtävät ohjelmassa voidaan suorittaa silmukoilla. Esimerkiksi
-        alla olevassa kuvassa robotti suorittaa kyykkyjä toistuvasti. Käsky
-        robotille voisi olla esimerkiksi "kyykkää toistuvasti kunnes käsketään
-        lopettaa".
+        Toistuvat tehtävät ohjelmassa voidaan suorittaa silmukoilla. Silmukat
+        ovat ns. toistorakenteita. Sanotaan esimerkiksi, että tahdomme alla
+        olevan kuvan robotin suorittavan kyykkyjä niin pitkään kuin halutaan.
+        Voisimme käskeä robottia "kyykkää, kyykkää..." toistuvasti kunnes
+        haluamme robotin lopettavan. Käskyn toistelun sijaan voisimme kuitenkin
+        käyttää silmukkaa. Miten tässä tapauksessa käsky muuttuisi?.
       </p>
       <img src="/Ohjelmointi/A59.gif"></img>
+      <p className="mb-4">
+        Tässä voisimme käyttää käskyä näin: "kyykkää, kunnes sanon lopeta".
+        Englanniksi tämä olisi "squat until I say stop" tai "Squat while I say
+        nothing" eli kyykkää kunnes toisin käsketään. Tämä on eräänlainen
+        esimerkki <code>while</code> silmukasta. Silmukka on ohjelmointirakenne,
+        joka toistaa koodilohkon niin kauan kuin ehto on tosi. Vinkki: voit
+        käyttää C-kielen harjoitteluun esimerkiksi tätä online-kääntäjää: Linkki
+        kääntäjään{" "}
+        <a href="https://www.programiz.com/c-programming/online-compiler/">
+          tässä
+        </a>
+      </p>
       <p className="mb-4">
         C-kielessä on kolme erilaista silmukkatyyppiä: <code>while</code>,{" "}
         <code>for</code> ja <code>do-while</code>.
@@ -43,7 +59,10 @@ while (i < 5) {
                 <p>
                   Tässä esimerkissä muuttuja <code>i</code> alustetaan arvoon 0
                   ja kasvaa yhdellä jokaisen iteraation jälkeen, kunnes se
-                  saavuttaa arvon 5.
+                  saavuttaa arvon 5. Silmukkaa toistetaan niin kauan kuin ehto
+                  on tosi eli niin kauan kuin <code>i</code> on pienempi kuin 5.
+                  Huomaa, että ehto tarkistetaan ennen koodilohkon
+                  suorittamista.
                 </p>
               </Card.Text>
             </Card.Body>
@@ -109,6 +128,53 @@ do {
           </Card>
         </Col>
       </Row>
+      <p>
+        <code>for</code> silmukka on monesti käytetyin silmukka kompaktin
+        esitystavan myötä ja koska alustus ja lopetusehdot vaaditaan niin se voi
+        ehkäistä ikuisten silmukoiden syntymistä.
+      </p>
+      <p>
+        Harjoitus: Kirjoita ohjelma, joka tulostaa kuukausien numerot{" "}
+        <code>for</code> -silmukalla.
+      </p>
+
+      <Button
+        onClick={() => setOpen(!open)}
+        aria-controls="example-collapse-text"
+        aria-expanded={open}
+        variant="primary"
+      >
+        Näytä/piilota esimerkkiratkaisu
+      </Button>
+      <Collapse in={open}>
+        <div id="example-collapse-text">
+          <Card className="mt-4">
+            <Card.Header>
+              <h4>while-silmukka</h4>
+            </Card.Header>
+            <Card.Body>
+              <Card.Text>
+                <p>
+                  <code>while</code> silmukka toistaa koodilohkon niin kauan
+                  kuin ehto on tosi.
+                </p>
+                <pre>
+                  <code>{`int i = 0;
+while (i < 13) {
+    printf("i on %d\\n", i);
+    i++;
+}`}</code>
+                </pre>
+                <p>
+                  Tässä esimerkissä muuttuja <code>i</code> alustetaan arvoon 0
+                  ja kasvaa yhdellä jokaisen iteraation jälkeen, kunnes se
+                  saavuttaa arvon 12.
+                </p>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+      </Collapse>
     </Container>
   );
 };
