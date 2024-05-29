f8a77478e011143a2653c03dedc396a8b300a803
Majava Ville <vikke.majava@gmail.com>
2024-05-29 17:02:28 +0300

    scrollabletext

--- src/ScrollableText.jsx
+++ src/ScrollableText.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { Card } from "react-bootstrap";
 import ReactMarkdown from "react-markdown";
 import rehypeKatex from "rehype-katex";
 import "katex/dist/katex.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const ScrollableText = ({ path }) => {
   const [text, setText] = useState("Ladataan tekstiä...");
@@ -17,18 +19,13 @@ const ScrollableText = ({ path }) => {
   }, []);
 
   return (
-    <div
-      style={{
-        overflow: "auto",
-        height: "200px",
-        border: "1px solid black",
-        padding: "10px",
-      }}
-    >
-      <pre style={{ whiteSpace: "pre-wrap" }}>
-        <ReactMarkdown rehypePlugins={[rehypeKatex]}>{text}</ReactMarkdown>
-      </pre>
-    </div>
+    <Card style={{ maxWidth: "100%", margin: "1rem auto" }}>
+      <Card.Body style={{ overflow: "auto", height: "200px" }}>
+        <pre style={{ whiteSpace: "pre-wrap", wordWrap: "break-word" }}>
+          <ReactMarkdown rehypePlugins={[rehypeKatex]}>{text}</ReactMarkdown>
+        </pre>
+      </Card.Body>
+    </Card>
   );
 };
 
